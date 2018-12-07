7088e107e11bcd7b2e3aba3c51d76a6f9f2bc2d8
ArtisterLuke <lukefan7928@gmail.com>
2018-12-07 18:23:12 +0800

    Delete NetworkRequestStatusListener.java

--- NetworkRequestStatusListener.java
+++
@@ -1,9 +0,0 @@
-package com.artist.wolf.networktool;
-
-public interface NetworkRequestStatusListener {
-
-    void onStatusChange(int status, boolean isSuccess);
-
-    void onProgressUpdate(int progress);
-
-}
