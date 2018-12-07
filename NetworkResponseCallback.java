896675a7583a5e214458789acb8d1d899378cffa
ArtisterLuke <lukefan7928@gmail.com>
2018-12-07 18:23:22 +0800

    Delete NetworkResponseCallback.java

--- NetworkResponseCallback.java
+++
@@ -1,7 +0,0 @@
-package com.artist.wolf.networktool;
-
-import com.artist.wolf.networktool.domain.Response;
-
-public abstract class NetworkResponseCallback  {
-    public abstract void onResponse(Response response);
-}
