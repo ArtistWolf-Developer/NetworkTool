2b45b492d66d33e06bd3330a755436f2486b033d
ArtisterLuke <lukefan7928@gmail.com>
2018-12-07 18:22:54 +0800

    Add files via upload

--- app/src/main/java/com/artist/wolf/networktool/NetworkResponseCallback.java
+++ app/src/main/java/com/artist/wolf/networktool/NetworkResponseCallback.java
@@ -3,5 +3,5 @@ package com.artist.wolf.networktool;
 import com.artist.wolf.networktool.domain.Response;
 
 public abstract class NetworkResponseCallback  {
-    protected abstract void onResponse(Response response);
+    public abstract void onResponse(Response response);
 }
